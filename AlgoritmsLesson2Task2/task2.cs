8b0dd05ec71637fe89647b4f79caa553e90164a1
yaNimotsi <yanimotsi@yanmdex.ru>
2021-02-26 22:04:41 +0300

    Add comments to task2

--- AlgoritmsLesson2Task2/task2.cs
+++ AlgoritmsLesson2Task2/task2.cs
@@ -2,13 +2,22 @@
 
 namespace AlgoritmsLesson2Task2
 {
-    class Program
+    class task2
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
-
+        //Если искомое число находится посередние то сложность составит 1
+        //Если же болшьше 1, то для каждого прохода while сложность будет сокращаться на половину
+        //Т.е. 1 круг n
+        //2 круг n/2
+        //3 круг n/2/2 т.е. n/2^2
+        //.
+        //.
+        //.
+        //для круга k это составит n/2^k
+        //Как это свести к Log(n), которое прозвучало на вебинаре - не понимаю. Видимо надо вспоминать школьный курс математики
         public static int BinarySearch(int[] inputArray, int searchValue)
         {
             int min = 0;
